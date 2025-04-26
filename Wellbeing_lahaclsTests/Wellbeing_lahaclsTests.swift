95c59c3b5cbafc843de53946c2c9a83a900751fe
Oscar Cooper Stern <oscarcs@g.ucla.edu>
2025-04-26 16:11:55 -0700

    Initial Commit

---
+++ Wellbeing_lahaclsTests/Wellbeing_lahaclsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wellbeing_lahaclsTests.swift
+//  Wellbeing_lahaclsTests
+//
+//  Created by Oscar Henry Cooper Stern on 4/26/25.
+//
+
+import Testing
+@testable import Wellbeing_lahacls
+
+struct Wellbeing_lahaclsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
